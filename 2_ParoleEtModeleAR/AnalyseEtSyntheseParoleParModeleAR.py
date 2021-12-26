0677ae5d83db1a949656491a33db09a6e526fd03
Christelle Randriamahefa <christelle.randriamahefa@gmail.com>
2021-12-26 11:54:09 +0100

    Ecoute du fichier initial

--- 2_ParoleEtModeleAR/AnalyseEtSyntheseParoleParModeleAR.py
+++ 2_ParoleEtModeleAR/AnalyseEtSyntheseParoleParModeleAR.py
@@ -1,12 +1,21 @@
-# -*- coding: utf-8 -*-
 """
-Created on Sun Sep 20 11:14:14 2020
-@contact: atto / abatt@univ-smb.fr 
+    MODULES
 """
-#%%
 from scipy.io import loadmat
 import sounddevice as sd
+import matplotlib.pyplot as plt 
+from scipy.signal import lfilter, hamming, freqz, deconvolve, convolve
+
+#Chargement de la donnée de parole
 DataParole = loadmat('DataParole.mat')
 DataParole = DataParole['DataParole']
 wait = input("Ajuster le volume - Puis Appuyer sur une touche du clavier pour continuer.")
-sd.play(DataParole, 8192) # son emis via haut parleur externe 
+sd.play(DataParole, 8192) # son emis via haut parleur externe
+
+#Visualisation de la donnée de parole
+z = DataParole
+plt.plot(z)
+plt.ylabel('Data Parole')
+plt.show() 
+
+
