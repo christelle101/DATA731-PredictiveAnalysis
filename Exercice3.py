00ef0ed0fd98e637fbdef3010e179c2f498bbf38
Christelle Randriamahefa <christelle.randriamahefa@gmail.com>
2021-12-30 18:01:39 +0100

    Question 4

--- Exercice3.py
+++ Exercice3.py
@@ -71,7 +71,7 @@ plt.show()
 
 def estimer_autocorrelation(x):
     """
-        Fonction qui permet d'estimer l'autocorrelation
+        #Fonction qui permet d'estimer l'autocorrelation
     """
     m = len(x)
     variance = x.var()
@@ -83,6 +83,10 @@ def estimer_autocorrelation(x):
 y= np.array(y)
 acfy = estimer_autocorrelation(y)
 
+
+"""
+    QUESTION 2 - Visualisation des spectres des sous-series
+"""
 def spectre(*args): 
     """
     Fonction qui permet de calculer les spectres des sous-series
@@ -111,7 +115,127 @@ plt.legend(['spectre1', 'spectre2','spectre3'])
 plt.title("Spectres")
 plt.show()
 
-sm.graphics.tsa.plot_acf(y[0:int(n/3)+1], lags=25)
+
+"""
+    QUESTION 2 - Visualisation de l'autocorrelation pour chaque serie temporelle
+"""
+#Visualisation de la serie 1
+sm.graphics.tsa.plot_acf(y[0:int(n/3)+1], lags = 40, color = '#EC3874')
 plt.title("Autocorrelation de la serie 1")
 plt.grid() 
 plt.show()
+
+#Visualisation de la serie 2
+sm.graphics.tsa.plot_acf(y[int(n/3)+1:2*int(n/3)], lags = 40)
+plt.grid() 
+plt.title("Autocorrelation de la serie 2")
+plt.show()
+
+#Visualisation de la serie 3
+sm.graphics.tsa.plot_acf(y[2*int(n/3)+1:n], lags = 40, color = '#4CAE58')
+plt.grid() 
+plt.title("Autocorrelation de la serie 3")
+plt.show()
+
+
+"""
+    QUESTION 3 - Creation d'une serie temporelle constituee par la somme des series
+    synthetisees precedemment.
+"""
+#Visualisation de y
+somme = []
+for j in range(len(y1)):
+  somme.append(y1[j] + y2[j] + y3[j])
+plt.plot(range(len(y1)),somme[:])
+plt.grid()
+plt.title("y : somme de y1, y2 et y3")
+plt.show()
+
+#Tracé de l'autocorrélation de y
+sm.graphics.tsa.plot_acf(somme, lags = 40)
+plt.grid() 
+plt.title("Autocorrelation de y")
+plt.show()
+
+#Tracé de la densité spectrale de puissance de y
+plt.psd(somme[:])
+plt.title("Densité spectrale de puissance de y")
+plt.show()
+
+
+"""
+    QUESTION 4 - Modélisation de y par un processus AR d'ordre 2.
+    L'objectif de cette etape est d'estimer les coefficients de ce modele et de comparer 
+    les autocorrélations/densites spectrales de y et du modele estime.
+"""
+t=range(-2,n-1)
+
+y=[k*0 for k in t]
+y1 = []
+y2 = []
+y3 = []
+for k in range(1,int(n/3)):
+    y[k]=-a[0]*y[k-1]-a[1]*y[k-2]+aleas.gauss(0,1)
+    y1.append(y[k])
+
+for k in range(int(n/3)+1,2*int(n/3)):
+    y[k]=-b[0]*y[k-1]-b[1]*y[k-2]+aleas.gauss(0,1)
+    y2.append(y[k])
+
+for k in range(2*int(n/3)+1,n):
+    y[k]=-c[0]*y[k-1]-c[1]*y[k-2]+aleas.gauss(0,1)
+    y3.append(y[k])
+
+y=y[3:]  # suppression des donnees transitoires
+t=t[3:]
+
+def AR_model_somme(debut, fin, serie, vrai_spectre):
+    """
+        : parametre debut : debut de l'intervalle
+        : parametre fin : fin de l'intervalle
+        : parametre serie : nom de la serie à modéliser
+        : parametre vrai_spectre : vrai spectre à comparer aux résultats 
+        : type debut : int
+        : type fin : int
+        : type serie : string
+        : type vrai_spectre : spectre
+        : return : la serie temporelle et la comparaison entre les spectres
+        : type return : plt.show
+    """
+    D = np.cov([
+        y[debut : fin] + [0, 0, 0, 0],
+        [0] + y[debut : fin] + [0, 0, 0],
+        [0, 0] + y[debut : fin] + [0, 0],
+        [0, 0, 0] + y[debut : fin] + [0],
+        [0, 0, 0, 0] + y[debut : fin]])
+    
+    E = - np.linalg.inv(D[0:2, 0:2]) @ D[0, 1:3].reshape(2, 1)  # car on veut l'avoir à l'ordre 2
+    H = - np.linalg.inv(D[0:3, 0:3]) @ D[0, 1:4].reshape(3, 1)  # car on veut l'avoir à l'ordre 3
+    E1 = np.append([1], E)  # vecteur de coefficients incluant a0(ordre 4)
+    H1 = np.append([1], H)
+    
+    #on trace la série entre 0 et le début de l'intervalle
+    plt.plot(t[debut : fin], y[debut : fin])
+    plt.title(serie)
+    plt.show()
+    
+    #on trace les spectres (estimation)
+    f, mag = spectre(E1, H1)
+    
+    #on calcule les valeurs correspondants aux spectres des 3 sous-series
+    plt.semilogy(
+    	f, mag[0],
+    	f, mag[1],
+    	':r',
+        f, vrai_spectre,':b',
+        linewidth = 2,
+    )
+    plt.title('Spectre / Calcul sur l intervalle [{} {}]'.format(debut, fin))
+    plt.legend(['ordre2', 'ordre3',"vrai spectre"])
+    return plt.show()
+
+AR_model_somme(0,int(n/3),"série 1",spectre1)
+AR_model_somme(int(n/3),2*int(n/3),"série 2",spectre2)
+AR_model_somme(0,n,"serie 3",spectre3)
+
+#Spectre de la somme de y1, y2, y3
